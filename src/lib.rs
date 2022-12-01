2d923785db22ca9c0fa7c1ab5588522ed36fb0f2
Brian Faga <brian@accidentaldevelopment.com>
2022-12-01 09:31:47 -0500

    chore: initial commit

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#![deny(clippy::all, clippy::pedantic, rust_2018_idioms)]
+
+// #[macro_use]
+// extern crate aoc_runner_derive;
+
+use aoc_runner_derive::aoc_lib;
+
+mod day1;
+
+aoc_lib! { year = 2022 }
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+#![deny(clippy::all, clippy::pedantic, rust_2018_idioms)]
+
+use aoc_runner_derive::aoc_main;
+
+aoc_main! { lib = aoc2022 }
